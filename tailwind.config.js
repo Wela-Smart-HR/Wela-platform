24621e7da3ab253cdd37776c1a98e5eed7d14bb4
Workflow <hearthpkh@gmail.com>
2026-02-12 23:35:59 +0700

    แก้ฟ้อน

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        heading: ['"Outfit"', '"Noto Sans Thai"', 'sans-serif'], // Fallback to Noto for Thai
         sans: ['"Noto Sans Thai"', 'sans-serif'],
+        mono: ['"Inter"', '"Noto Sans Thai"', 'monospace'], // Fallback to Noto for Thai
       },
     },
   },
